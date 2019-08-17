6447b4abb2f7b2c07af18406a144cd5c5a08b3c2
Subarna Sahoo <48875593+subarna-sahoo@users.noreply.github.com>
2019-08-17 19:07:21 +0530

    Add files via upload

--- mainProg.py
+++ mainProg.py
@@ -1,73 +1,302 @@
 import random
+from random import randint
+import time
 
-import time                 #for use of time.sleep
 
-Glis=[0,0,0,0,0]
 
-def Rand(start=10, end=35, num=5):
-    lis1 = []
-    lis2 = []
-    lis3 = []
-    lis4 = []
-    lis5 = []
-    for j in range(num):
-        lis1.append(random.randint(start, end))
-        lis2.append(random.randint(start, end))
-        lis3.append(random.randint(start, end))
-        lis4.append(random.randint(start, end))
-        lis5.append(random.randint(start, end))
-    B = [lis1[0],lis2[0],lis3[0],lis4[0],lis5[0]]
-    I = [lis1[1],lis2[1],lis3[1],lis4[1],lis5[1]]
-    N = [lis1[2],lis2[2],lis3[2],lis4[2],lis5[2]]
-    G = [lis1[3],lis2[3],lis3[3],lis4[3],lis5[3]]
-    O = [lis1[4],lis2[4],lis3[4],lis4[4],lis5[4]]
-    print('Your Card: \n')
-    def check_card():
-        card = '''                 B   I   N   G   O
-                 {}  {}  {}  {}  {}
-                 {}  {}  {}  {}  {}
-                 {}  {}  {}  {}  {}
-                 {}  {}  {}  {}  {}
-                 {}  {}  {}  {}  {}
+comp_li=[0,0,0,0,0]
+player_names=[]
+
+def Card(start=10, end=20, num=12):
+    B = []
+    I = []
+    N = []
+    G = []
+    O = []
+    
+    for draw in range(num):
+        if len(B) != 5:
+            element=randint(start,end)
+            if not element in B:
+                B.append(element)
+    for draw in range(num):
+        if len(I) != 5:
+            element=randint(start,end)
+            if not element in I:
+                I.append(element)
+    for draw in range(num):
+        if len(N) != 5:
+            element=randint(start,end)
+            if not element in N:
+                N.append(element)
+    for draw in range(num):
+        if len(G) != 5:
+            element=randint(start,end)
+            if not element in G:
+                G.append(element)
+    for draw in range(num):
+        if len(O) != 5:
+            element=randint(start,end)
+            if not element in O:
+                O.append(element)
+
+    def Check_Card():
+        card = '''
+               B - {}  {}  {}  {}  {}
+               
+               I - {}  {}  {}  {}  {}
+               
+               N - {}  {}  {}  {}  {}
+               
+               G - {}  {}  {}  {}  {}
+               
+               O - {}  {}  {}  {}  {}
                 '''
-        print (card.format(lis1[0],lis1[1],lis1[2],lis1[3],lis1[4],
-                           lis2[0],lis2[1],lis2[2],lis2[3],lis2[4],
-                           lis3[0],lis3[1],lis3[2],lis3[3],lis3[4],
-                           lis4[0],lis4[1],lis4[2],lis4[3],lis4[4],
-                           lis5[0],lis5[1],lis5[2],lis5[3],lis5[4]))
-    check_card()
-    bingo = int(input('Enter Bingos: '))    #you can take it randomly,as player's
+        try:
+            print (card.format(B[0],B[1],B[2],B[3],B[4],
+                               I[0],I[1],I[2],I[3],I[4],
+                               N[0],N[1],N[2],N[3],N[4],
+                               G[0],G[1],G[2],G[3],G[4],
+                               O[0],O[1],O[2],O[3],O[4]))
+        except (ValueError, IndexError):
+            pass
+    
+    print('Your Card: \n')
+    Check_Card()
+    
+    input('PRESS ENTER TO START THE GAME.\n')
+    bingo = 3#int(input('Enter Bingos: '))
+    
+    #for one time exicution of rows/column/diagonal
+    count_B = 0
+    count_I = 0
+    count_N = 0
+    count_G = 0
+    count_O = 0
+    count_V1 = 0
+    count_V2 = 0
+    count_V3 = 0
+    count_V4 = 0
+    count_V5 = 0
+    count_D1 = 0
+    count_D2 = 0
+    
+    global_call =[]
     while bingo > 0:
-        print(bingo,'Bingos left.')
-        bingo -= 1                          # This is the same as count = bingo + 1
-        #-----------
-        ltr = ['B', 'I', 'N', 'G', 'O']
-        for draw in range(5):    
-            drawn_numbers=list()
-            bingo_draw=random.randint(10,35)
-            if not bingo_draw in drawn_numbers:
-                print((random.choice(ltr)),',',(bingo_draw))
+        try:
+            list_names = ['B', 'I', 'N', 'G', 'O']      
+            for draw in range(200):    
+                random_name =(random.choice(list_names))   
+                element=(random.randint(10,30))
+                call = random_name,element
+                if not call in global_call:
+                    if bingo>0:
+                        print(call)
+                        global_call.append(call)
+                    else:
+                        pass
+                else:
+                    pass
+                time.sleep(0.5)
                 print('\n')
-                drawn_numbers.append(bingo_draw)
-                time.sleep(1.5)                     #import time
-                check_card()
 
-        #-----------    
+                #----------------------------
+                V1=[B[0],I[0],N[0],G[0],O[0]]
+                V2=[B[1],I[1],N[1],G[1],O[1]]
+                V3=[B[2],I[2],N[2],G[2],O[2]]
+                V4=[B[3],I[3],N[3],G[3],O[3]]
+                V5=[B[4],I[4],N[4],G[4],O[4]]
 
-# Game Index
-def main_menu():
-    print('''
-           <<  Wllcome to Bingo Python >>
-           
-               < A > For New Game.     
-           
-               < B > For Check Score.
-           ''')
-    inp=input(':')
-    if (inp=='A'):
-        Rand()
-    elif (inp=='B'):
-        print('No New Scores Yet')
+                #----------------------------
+                D1=[B[0],I[1],N[2],G[3],O[4]]
+                D2=[B[4],I[3],N[2],G[1],O[0]]
+
+                #----------------------------
+                if random_name == 'B':
+                    index_value = B.index(element)
+                    B[index_value] = 0
+                    print ('after changed by machine : ')
+                    Check_Card()
+                elif random_name=='I':
+                    index_value = I.index(element)
+                    I[index_value] = 0
+                    print ('after changed by machine : ')
+                    Check_Card()
+                elif random_name=='N':
+                    index_value = N.index(element)
+                    N[index_value] = 0
+                    print ('after changed by machine : ')
+                    Check_Card()
+                elif random_name=='G': 
+                    index_value = G.index(element)
+                    G[index_value] = 0
+                    print ('after changed by machine : ')
+                    Check_Card()
+                elif random_name=='O':
+                    index_value = O.index(element)
+                    O[index_value] = 0
+                    print ('after changed by machine : ')
+                    Check_Card()
+                else:
+                    print('End Game.')
+                    
+                # Remove Bingo
+                # Horizontal >>
+                if  (bingo > 0 and count_B==0 and comp_li == B):
+                    bingo -=1
+                    count_B += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                
+                elif  (bingo > 0 and count_I==0 and comp_li == I):
+                    bingo -=1
+                    count_I += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                elif  (bingo > 0 and count_N==0 and comp_li == N):
+                    bingo -=1
+                    count_N += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                elif  (bingo > 0 and count_G==0 and comp_li == G):
+                    bingo -=1
+                    count_G += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                elif  (bingo > 0 and count_O==0 and comp_li == O):
+                    bingo -=1
+                    count_O += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                # Vertical >>
+                elif  (bingo > 0 and count_V1==0 and comp_li == V1):
+                    bingo -=1
+                    count_V1 += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                elif  (bingo > 0 and count_V2==0 and comp_li == V2):
+                    bingo -=1
+                    count_V2 += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                elif  (bingo > 0 and count_V3==0 and comp_li == V3):
+                    bingo -=1
+                    count_V3 += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                elif  (bingo > 0 and count_V4==0 and comp_li == V4):
+                    bingo -=1
+                    count_V4 += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                elif  (bingo > 0 and count_V2==0 and comp_li == V5):
+                    bingo -=1
+                    count_V2 += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+                    
+                # Diagonal >>
+                elif  (bingo > 0 and count_D1==0 and comp_li == D1):
+                    bingo -=1
+                    count_D1 += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+
+                elif  (bingo > 0 and count_D2==0 and comp_li == D2):
+                    bingo -=1
+                    count_D2 += 1
+                    print("bingoleft",bingo)
+                    Check_Card()
+                    time.sleep(2)
+         
+                else:
+                    pass
+                time.sleep(0)
+                
+        except ValueError:
+            pass
+
+    #====================================================
+    print('                 End Game                 ')
+    #====================================================
+
+
+
+    
+
+# Game Index-------------------------------------------------------------
+#==============================SINGLE PLAYER=============================
+def single_player():
+    player_name = input ("Enter Your Name: ")
+    if not player_name in player_names:
+        player_names.append(player_name)
+        print('New Player Added: ',player_name)
+        Card()
     else:
-        print('wrong input')
+        print('''
+                 A player with same name already exist.   !!!
+                ''')
+        single_player()
+
+#==============================MULTI PLAYER============================
+def multi_player():
+    r=int(input('how many player :'))
+    for player in range(r):
+        name = input('Player Name: ')
+        player_names.append(name)
+        print('\n\nNext Player Name:\n ')
+    print('You have Entered: ',player_names)
+    game=input('\nPress ENTER for Continue.. \n')
+    for i in player_names:
+        print (i)
+        Card()
+    sgame=input('Press Enter for Start The Game >')
+    startgame()
+    
+
+#==============================MAIN MENU============================
+def main_menu():
+    inp=input('''
+ <<<<<<<<<<   Wllcome to Bingo Python  >>>>>>>>>>>>>
+           
+               < 1 > For New Game.     
+           
+               < 2 > For Check Score.
+           :-''')
+    if (inp=='1'):
+        in_pl = input('''
+                < 1 >  For SIngle Player.
+                
+                < 2 >  For Multiplayer.
+           :-''')
+        if (in_pl=='1'):
+            single_player()
+        elif (in_pl=='2'):
+            multi_player()
+    elif (inp=='2'):
+        print(' \n\n      !!!!        No New Scores Yet ("_")       !!!!\n\n                  LETS MAKE ONE  >>\n\n')
+        main_menu()
+    else:
+        print('\n\n  !!!!!!!!!!!        Wrong input       !!!!!!!!!!!!    ')
+        main_menu()
+#=============================**==============================
 main_menu()
